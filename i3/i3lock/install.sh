a18d50d4a3177ea4983d62b6bc78d6657b2d1b85
Patrick Willner <patrickwillner@gmx.de>
2022-10-23 17:13:35 +0200

    update

---
+++ i3/i3lock/install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp ./lock.sh /usr/bin/i3lock-blurred
---
+++ sync.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ~/.config/i3 i3
+cp -r ~/.config/alacritty alacritty
+cp ~/.tmux.conf tmux/.tmux.conf
+cp ~/.config/nvim/init.vim nvim/init.vim
