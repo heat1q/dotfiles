3f579a8c5d9839a25a024e88f5625968525eb3e2
Patrick Willner <50421879+heat1q@users.noreply.github.com>
2022-11-23 11:57:25 +0100

    update sync script

--- sync.sh
+++ sync.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-cp -r ~/.config/i3 .
-cp -r ~/.config/alacritty .
-cp ~/.tmux.conf tmux/.tmux.conf
-cp ~/.config/nvim/init.vim nvim/init.vim
+cmd=$1
+
+if [[ "$1" == "diff" ]]; then
+    diff -ruN --color=always ~/.tmux.conf tmux/.tmux.conf
+    diff -ruN --color=always ~/.config/nvim/init.vim nvim/init.vim
+elif [[ "$1" == "get" ]]; then
+    cp -r ~/.config/i3 .
+    cp -r ~/.config/alacritty .
+    cp ~/.tmux.conf tmux/.tmux.conf
+    cp ~/.config/nvim/init.vim nvim/init.vim
+elif [[ "$1" == "cp" ]]; then
+    if [[ -f "~/.config/i3" ]]; then
+        mv ~/.config/i3 ~/.config/i3.backup
+        cp ./i3 ~/.config/
+    fi
+
+    if [[ -f "~/.config/alacritty" ]]; then
+        mv ~/.config/alacritty ~/.config/alacritty.backup
+        cp ./alacritty ~/.config/
+    fi
+
+    mv ~/.tmux.conf ~/.tmux.conf.backup 
+    cp ./tmux/.tmux.conf ~/.tmux.conf
+
+    mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.backup
+    cp ./nvim/init.vim ~/.config/nvim/init.vim
+fi
