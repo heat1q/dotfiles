5459c59e6be9899ba295b926c3ef6468bfc7313b
Patrick Willner <50421879+heat1q@users.noreply.github.com>
2023-06-13 23:10:27 +0200

    fix: i3 display settings

--- i3/scripts/display.sh
+++ i3/scripts/display.sh
@@ -2,12 +2,23 @@
 
 # KERNEL=="card0", ACTION=="change", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/patrick/.Xauthority", RUN+="/opt/scripts/display.sh"
 
-built_in="$(xrandr | grep " connected" | grep "eDP" | awk '{print $1}')"
-ext_display="$(xrandr | grep " connected" | grep -v "eDP-1" | awk '{print $1}')"
-
-if [[ -n "$ext_display" ]]; then
-	xrandr --output $built_in --auto --output $ext_display --auto --right-of eDP-1
-else
-	xrandr --output $built_in --auto 
-	xrandr | grep disconnected | awk '{print $1}' | xargs -I {} xrandr --output {} --off
+if [[ "$DISPLAY_LOCK" == "locked" ]]; then
+    exit 0
 fi
+
+export DISPLAY_LOCK=locked
+built_in="$(xrandr | grep " connected" | grep "eDP" | awk '{print $1}')"
+echo $built_in
+ext_display="$(xrandr | grep " connected" | grep -v "eDP-1" | awk '{print $1}')"
+echo $ext_display
+if [[ -n "$ext_display" ]]; then
+	xrandr --output $built_in --auto --output $ext_display --auto --right-of eDP-1 >/dev/null
+else
+	xrandr --output $built_in --auto >/dev/null
+	xrandr | grep disconnected | awk '{print $1}' | xargs -I {} xrandr --output {} --off >/dev/null
+fi
+
+sleep 5
+
+# Set the lock to prevent multiple executions
+export DISPLAY_LOCK=unlocked
